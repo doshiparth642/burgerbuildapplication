89ce520b89ac6308cb6c9b27043c82daee6134b7
doshiparth642 <doshiparth642@gmail.com>
2022-08-02 16:31:44 +0530

    'fhfhhff'

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -24,7 +24,7 @@ const Burger = (props) => {
     return (
         <div className='Burger'>
             <BurgerIngradiant type='bread-top' />
-            <p>Changes in new branch</p>
+            <p>Changes in updated branch</p>
             {transformedIngredients}
             <BurgerIngradiant type='bread-bottom' />
 
