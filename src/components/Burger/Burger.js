1badf38b3d5d00b0da44203867406451c72b041b
doshiparth642 <doshiparth642@gmail.com>
2022-08-02 15:34:33 +0530

    'changes_master'

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,30 +2,31 @@ import React from "react";
 import './Burger.css';
 import BurgerIngradiant from "./BurgerIngrediant/BurgerIngradiant";
 
-const Burger = (props) =>{
+const Burger = (props) => {
 
-     let transformedIngredients = Object.keys(props.ingredients)
-        .map(igKey=>{
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
 
-            return [...Array(props.ingredients[igKey])].map((_,i)=>{
-                return <BurgerIngradiant key={igKey+i} type={igKey}/>;
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngradiant key={igKey + i} type={igKey} />;
             })
-      
+
         })
-        .reduce((arr,el)=>{
-          return arr.concat(el)
-        },[])
-        console.log(transformedIngredients);
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, [])
+    console.log(transformedIngredients);
 
-        if(transformedIngredients.length === 0){
-            transformedIngredients = <p>Please start adding ingredients!</p>
-        }
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
-    return(
+    return (
         <div className='Burger'>
-        <BurgerIngradiant type='bread-top' />
-       {transformedIngredients}
-        <BurgerIngradiant type='bread-bottom'/>
+            <BurgerIngradiant type='bread-top' />
+            <p>Changes in master branch</p>
+            {transformedIngredients}
+            <BurgerIngradiant type='bread-bottom' />
 
         </div>
     );
